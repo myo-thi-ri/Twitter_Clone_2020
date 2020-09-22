d7c5ce4164c120bcf55e324acc95433d8ac78aa2
MYO THIRI <sandarlamin.llm@gmail.com>
2020-09-23 01:17:36 +0900

    Initial commit.Create the skeleton structure of twitter frontend.

---
+++ js/index.js
@@ -0,0 +1,54 @@
+const URL = "http://localhost:3000/tweets";
+
+/**
+ * Retrive Twitter Data from API
+ */
+const getTwitterData = () => {
+    const url = "http://localhost:3000/tweets?q=coding&count=10"
+    fetch(url).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data)
+    })
+}
+
+
+
+/**
+ * Save the next page data
+ */
+const saveNextPage = (metadata) => {
+}
+
+/**
+ * Handle when a user clicks on a trend
+ */
+const selectTrend = (e) => {
+}
+
+/**
+ * Set the visibility of next page based on if there is data on next page
+ */
+const nextPageButtonVisibility = (metadata) => {
+}
+
+/**
+ * Build Tweets HTML based on Data from API
+ */
+const buildTweets = (tweets, nextPage) => {
+
+}
+
+/**
+ * Build HTML for Tweets Images
+ */
+const buildImages = (mediaList) => {
+
+}
+
+/**
+ * Build HTML for Tweets Video
+ */
+const buildVideo = (mediaList) => {
+
+}
